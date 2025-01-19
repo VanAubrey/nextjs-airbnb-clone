ad1d2598353a5aec56c521cbb5b1edef64877e64
VanAubrey <vanaubreyd19@gmail.com>
2025-01-19 21:01:01 +0800

    init: create nextjs app and add tailwind

---
+++ airbnb-clone/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Nunito } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
