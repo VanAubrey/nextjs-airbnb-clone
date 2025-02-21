b43446c95db632b27f57d783932c90334ded949d
Van <115492319+VanAubrey@users.noreply.github.com>
2025-02-21 23:03:16 +0800

    feat: create reusable components (#1)

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 import { useCallback, useState } from "react";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -43,7 +45,7 @@ const UserMenu = () => {
                 label="Login"
               />
               <MenuItem 
-                onclick={() => {}}
+                onclick={registerModal.onOpen}
                 label="Sign up"
               />
             </>
