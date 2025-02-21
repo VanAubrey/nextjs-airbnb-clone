07bad15ac97aa03bb5ec3207c7f7a6f465712b01
VanAubrey <vanaubreyd19@gmail.com>
2025-02-21 23:00:40 +0800

    feat: create reusable components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <Navbar />
+          <RegisterModal />
         </ClientOnly>
         {children}
       </body>
