fa105308b75bdb8f9160d92c82d79f69ed293141
VanAubrey <vanaubreyd19@gmail.com>
2025-02-26 22:44:32 +0800

    feat: add rentModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import { Nunito } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
+
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+import RentModal from "./components/modals/RentModal";
+import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata: Metadata = {
@@ -29,9 +31,10 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <Navbar currentUser={currentUser} />
+          <RentModal />
           <LoginModal />
           <RegisterModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
